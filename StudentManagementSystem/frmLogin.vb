d4a20c473745af7d431d73d3df37ea7d9d724295
Talha <Talha@TALHA-LENOVO>
2019-09-05 19:25:35 +0200

    Add project files.

---
+++ StudentManagementSystem/frmLogin.vb
@@ -0,0 +1,17 @@
+﻿Public Class frmLogin
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub pbxStudent_Click(sender As Object, e As EventArgs) Handles pbxStudent.Click
+        lblUsername.Text = "Student Number: "
+    End Sub
+
+    Private Sub pbxStaff_Click(sender As Object, e As EventArgs) Handles pbxStaff.Click
+        lblUsername.Text = "Staff Number: "
+    End Sub
+End Class

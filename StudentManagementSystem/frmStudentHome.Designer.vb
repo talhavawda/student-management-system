4181642736ee11d70dd2f96c97260461a51ffb01
Talha <218023210@stu.ukzn.ac.za>
2019-09-11 14:13:54 +0200

    changed button to pictureBoxes

--- StudentManagementSystem/frmStudentHome.Designer.vb
+++ StudentManagementSystem/frmStudentHome.Designer.vb
@@ -23,42 +23,39 @@ Partial Class frmStudentHome
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
 		Me.Panel1 = New System.Windows.Forms.Panel()
-		Me.Button2 = New System.Windows.Forms.Button()
-		Me.Button3 = New System.Windows.Forms.Button()
-		Me.pbDetails = New System.Windows.Forms.PictureBox()
 		Me.Label1 = New System.Windows.Forms.Label()
+		Me.pbDetails = New System.Windows.Forms.PictureBox()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.PictureBox2 = New System.Windows.Forms.PictureBox()
 		Me.Panel1.SuspendLayout()
 		CType(Me.pbDetails, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
 		'
 		'Panel1
 		'
+		Me.Panel1.Controls.Add(Me.Label3)
+		Me.Panel1.Controls.Add(Me.PictureBox2)
+		Me.Panel1.Controls.Add(Me.Label2)
+		Me.Panel1.Controls.Add(Me.PictureBox1)
 		Me.Panel1.Controls.Add(Me.Label1)
 		Me.Panel1.Controls.Add(Me.pbDetails)
-		Me.Panel1.Controls.Add(Me.Button3)
-		Me.Panel1.Controls.Add(Me.Button2)
 		Me.Panel1.Location = New System.Drawing.Point(22, 12)
 		Me.Panel1.Name = "Panel1"
 		Me.Panel1.Size = New System.Drawing.Size(597, 340)
 		Me.Panel1.TabIndex = 0
 		'
-		'Button2
+		'Label1
 		'
-		Me.Button2.Location = New System.Drawing.Point(222, 91)
-		Me.Button2.Name = "Button2"
-		Me.Button2.Size = New System.Drawing.Size(150, 120)
-		Me.Button2.TabIndex = 1
-		Me.Button2.Text = "Registration"
-		Me.Button2.UseVisualStyleBackColor = True
-		'
-		'Button3
-		'
-		Me.Button3.Location = New System.Drawing.Point(403, 91)
-		Me.Button3.Name = "Button3"
-		Me.Button3.Size = New System.Drawing.Size(150, 120)
-		Me.Button3.TabIndex = 2
-		Me.Button3.Text = "Results"
-		Me.Button3.UseVisualStyleBackColor = True
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(44, 221)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(107, 13)
+		Me.Label1.TabIndex = 4
+		Me.Label1.Text = "View/Change Details"
 		'
 		'pbDetails
 		'
@@ -69,14 +66,41 @@ Partial Class frmStudentHome
 		Me.pbDetails.TabIndex = 3
 		Me.pbDetails.TabStop = False
 		'
-		'Label1
+		'Label2
 		'
-		Me.Label1.AutoSize = True
-		Me.Label1.Location = New System.Drawing.Point(44, 221)
-		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(107, 13)
-		Me.Label1.TabIndex = 4
-		Me.Label1.Text = "View/Change Details"
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(243, 221)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(63, 13)
+		Me.Label2.TabIndex = 6
+		Me.Label2.Text = "Registration"
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.PictureBox1.Location = New System.Drawing.Point(229, 91)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(150, 120)
+		Me.PictureBox1.TabIndex = 5
+		Me.PictureBox1.TabStop = False
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(445, 221)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(42, 13)
+		Me.Label3.TabIndex = 8
+		Me.Label3.Text = "Results"
+		'
+		'PictureBox2
+		'
+		Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.PictureBox2.Location = New System.Drawing.Point(431, 91)
+		Me.PictureBox2.Name = "PictureBox2"
+		Me.PictureBox2.Size = New System.Drawing.Size(150, 120)
+		Me.PictureBox2.TabIndex = 7
+		Me.PictureBox2.TabStop = False
 		'
 		'frmStudentHome
 		'
@@ -89,13 +113,17 @@ Partial Class frmStudentHome
 		Me.Panel1.ResumeLayout(False)
 		Me.Panel1.PerformLayout()
 		CType(Me.pbDetails, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.ResumeLayout(False)
 
 	End Sub
 
 	Friend WithEvents Panel1 As Panel
-	Friend WithEvents Button3 As Button
-	Friend WithEvents Button2 As Button
 	Friend WithEvents pbDetails As PictureBox
 	Friend WithEvents Label1 As Label
+	Friend WithEvents Label3 As Label
+	Friend WithEvents PictureBox2 As PictureBox
+	Friend WithEvents Label2 As Label
+	Friend WithEvents PictureBox1 As PictureBox
 End Class
--- StudentManagementSystem/frmStudentHome.vb
+++ StudentManagementSystem/frmStudentHome.vb
@@ -2,4 +2,8 @@
 	Private Sub Button1_Click(sender As Object, e As EventArgs)
 		'change text of Main
 	End Sub
+
+	Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+	End Sub
 End Class

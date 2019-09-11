786358a1807196cd5b21d8c103129f75f85f8a5f
Talha <218023210@stu.ukzn.ac.za>
2019-09-11 16:11:19 +0200

    Wednesday Afternoon Final

--- StudentManagementSystem/frmSplashScreen.Designer.vb
+++ StudentManagementSystem/frmSplashScreen.Designer.vb
@@ -1,92 +1,76 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmSplashScreen
-    Inherits System.Windows.Forms.Form
+	Inherits System.Windows.Forms.Form
 
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
-    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
-    End Sub
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
 
-    'Required by the Windows Form Designer
-    Private components As System.ComponentModel.IContainer
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
-    Private Sub InitializeComponent()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
-        Me.lblProgressPercent = New System.Windows.Forms.Label()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Location = New System.Drawing.Point(199, 48)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(142, 148)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(248, 220)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(27, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "by []"
-        '
-        'ProgressBar
-        '
-        Me.ProgressBar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ProgressBar.Location = New System.Drawing.Point(119, 256)
-        Me.ProgressBar.Name = "ProgressBar"
-        Me.ProgressBar.Size = New System.Drawing.Size(303, 23)
-        Me.ProgressBar.Step = 1
-        Me.ProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous
-        Me.ProgressBar.TabIndex = 2
-        '
-        'lblProgressPercent
-        '
-        Me.lblProgressPercent.AutoSize = True
-        Me.lblProgressPercent.Location = New System.Drawing.Point(247, 260)
-        Me.lblProgressPercent.Name = "lblProgressPercent"
-        Me.lblProgressPercent.Size = New System.Drawing.Size(0, 13)
-        Me.lblProgressPercent.TabIndex = 3
-        '
-        'frmSplashScreen
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.ControlDark
-        Me.ClientSize = New System.Drawing.Size(541, 323)
-        Me.Controls.Add(Me.lblProgressPercent)
-        Me.Controls.Add(Me.ProgressBar)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-        Me.Name = "frmSplashScreen"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSplashScreen))
+		Me.lblProgressPercent = New System.Windows.Forms.Label()
+		Me.prbSplash = New System.Windows.Forms.ProgressBar()
+		Me.tmrSplash = New System.Windows.Forms.Timer(Me.components)
+		Me.SuspendLayout()
+		'
+		'lblProgressPercent
+		'
+		Me.lblProgressPercent.AutoSize = True
+		Me.lblProgressPercent.Location = New System.Drawing.Point(247, 260)
+		Me.lblProgressPercent.Name = "lblProgressPercent"
+		Me.lblProgressPercent.Size = New System.Drawing.Size(0, 13)
+		Me.lblProgressPercent.TabIndex = 3
+		'
+		'prbSplash
+		'
+		Me.prbSplash.BackColor = System.Drawing.SystemColors.Control
+		Me.prbSplash.Location = New System.Drawing.Point(144, 260)
+		Me.prbSplash.Name = "prbSplash"
+		Me.prbSplash.Size = New System.Drawing.Size(259, 23)
+		Me.prbSplash.Step = 1
+		Me.prbSplash.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+		Me.prbSplash.TabIndex = 4
+		'
+		'tmrSplash
+		'
+		Me.tmrSplash.Interval = 1
+		'
+		'frmSplashScreen
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.SystemColors.ControlDark
+		Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+		Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.ClientSize = New System.Drawing.Size(541, 323)
+		Me.Controls.Add(Me.prbSplash)
+		Me.Controls.Add(Me.lblProgressPercent)
+		Me.DoubleBuffered = True
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+		Me.Name = "frmSplashScreen"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "Form1"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-    End Sub
-
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents ProgressBar As ProgressBar
-    Friend WithEvents lblProgressPercent As Label
+	End Sub
+	Friend WithEvents lblProgressPercent As Label
+	Friend WithEvents prbSplash As ProgressBar
+	Friend WithEvents tmrSplash As Timer
 End Class

bd65542fae8f7533bb6d31ebe1cabd38c09684af
Azhar Mohamed <Azhar Mohamed@DESKTOP-L5VNSL1>
2019-09-11 14:43:27 +0200

    TEST 2

--- StudentManagementSystem/frmStudentHome.Designer.vb
+++ StudentManagementSystem/frmStudentHome.Designer.vb
@@ -22,80 +22,68 @@ Partial Class frmStudentHome
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
-		Me.Panel1 = New System.Windows.Forms.Panel()
-		Me.Button2 = New System.Windows.Forms.Button()
-		Me.Button3 = New System.Windows.Forms.Button()
-		Me.pbDetails = New System.Windows.Forms.PictureBox()
-		Me.Label1 = New System.Windows.Forms.Label()
-		Me.Panel1.SuspendLayout()
-		CType(Me.pbDetails, System.ComponentModel.ISupportInitialize).BeginInit()
-		Me.SuspendLayout()
-		'
-		'Panel1
-		'
-		Me.Panel1.Controls.Add(Me.Label1)
-		Me.Panel1.Controls.Add(Me.pbDetails)
-		Me.Panel1.Controls.Add(Me.Button3)
-		Me.Panel1.Controls.Add(Me.Button2)
-		Me.Panel1.Location = New System.Drawing.Point(22, 12)
-		Me.Panel1.Name = "Panel1"
-		Me.Panel1.Size = New System.Drawing.Size(597, 340)
-		Me.Panel1.TabIndex = 0
-		'
-		'Button2
-		'
-		Me.Button2.Location = New System.Drawing.Point(222, 91)
-		Me.Button2.Name = "Button2"
-		Me.Button2.Size = New System.Drawing.Size(150, 120)
-		Me.Button2.TabIndex = 1
-		Me.Button2.Text = "Registration"
-		Me.Button2.UseVisualStyleBackColor = True
-		'
-		'Button3
-		'
-		Me.Button3.Location = New System.Drawing.Point(403, 91)
-		Me.Button3.Name = "Button3"
-		Me.Button3.Size = New System.Drawing.Size(150, 120)
-		Me.Button3.TabIndex = 2
-		Me.Button3.Text = "Results"
-		Me.Button3.UseVisualStyleBackColor = True
-		'
-		'pbDetails
-		'
-		Me.pbDetails.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-		Me.pbDetails.Location = New System.Drawing.Point(30, 91)
-		Me.pbDetails.Name = "pbDetails"
-		Me.pbDetails.Size = New System.Drawing.Size(150, 120)
-		Me.pbDetails.TabIndex = 3
-		Me.pbDetails.TabStop = False
-		'
-		'Label1
-		'
-		Me.Label1.AutoSize = True
-		Me.Label1.Location = New System.Drawing.Point(44, 221)
-		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(107, 13)
-		Me.Label1.TabIndex = 4
-		Me.Label1.Text = "View/Change Details"
-		'
-		'frmStudentHome
-		'
-		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(644, 401)
-		Me.Controls.Add(Me.Panel1)
-		Me.Name = "frmStudentHome"
-		Me.Text = "Home | Student"
-		Me.Panel1.ResumeLayout(False)
-		Me.Panel1.PerformLayout()
-		CType(Me.pbDetails, System.ComponentModel.ISupportInitialize).EndInit()
-		Me.ResumeLayout(False)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.pbDetails = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        CType(Me.pbDetails, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.pbDetails)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Location = New System.Drawing.Point(22, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(597, 340)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 221)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "View/Change Details"
+        '
+        'pbDetails
+        '
+        Me.pbDetails.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbDetails.Location = New System.Drawing.Point(30, 91)
+        Me.pbDetails.Name = "pbDetails"
+        Me.pbDetails.Size = New System.Drawing.Size(150, 120)
+        Me.pbDetails.TabIndex = 3
+        Me.pbDetails.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(403, 91)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(150, 120)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Results"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'frmStudentHome
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(644, 401)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "frmStudentHome"
+        Me.Text = "Home | Student"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.pbDetails, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
 
-	End Sub
+    End Sub
 
-	Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel1 As Panel
 	Friend WithEvents Button3 As Button
-	Friend WithEvents Button2 As Button
-	Friend WithEvents pbDetails As PictureBox
-	Friend WithEvents Label1 As Label
+    Friend WithEvents pbDetails As PictureBox
+    Friend WithEvents Label1 As Label
 End Class

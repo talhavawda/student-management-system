07471f76431b98826df81b7923919e701acfef4f
Luqmaan <luqhaf5@gmail.com>
2019-09-22 10:36:02 +0200

    Fixed error in new student, choosing majors

--- StudentManagementSystem/frmSplashScreen.vb
+++ StudentManagementSystem/frmSplashScreen.vb
@@ -1,15 +1,14 @@
 ﻿Public Class frmSplashScreen
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) 
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
 
     End Sub
 
-	Private Sub frmSplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-		'My.Application.MinimumSplashScreenDisplayTime = 3000	'this must be set in ApplicationEvents.vb (accessible via Project Properties)
-		frmTeamScreen.Show()
+    Private Sub frmSplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'My.Application.MinimumSplashScreenDisplayTime = 3000	'this must be set in ApplicationEvents.vb (accessible via Project Properties)
+        frmTeamScreen.Show()
 
-		tmrLoadingLabel.Enabled = True
-
-	End Sub
+        tmrLoadingLabel.Enabled = True
+    End Sub
 
     Private Sub tmrSplash_Tick(sender As Object, e As EventArgs)
 
@@ -18,23 +17,18 @@
     End Sub
 
     Private Sub tmrLoadingLabel_Tick(sender As Object, e As EventArgs) Handles tmrLoadingLabel.Tick
-		'Since this form is set as a Splash Screen in the Project Settings the for will automatically close after 2 seconds thus no need to check for length of the LoadingBar 
-		lblLoadingBar.Text += "|"
+        'Since this form is set as a Splash Screen in the Project Settings the for will automatically close after 2 seconds thus no need to check for length of the LoadingBar 
+        lblLoadingBar.Text += "|"
+        If lblLoadingBar.Text = "||" Then
+            Threading.Thread.Sleep(1000)
+            frmTeamScreen.Close()
+        End If
+        'now SplashScreen will show for another 2000ms as MinDisplayTime set to 3000
 
-		If lblLoadingBar.Text = "||" Then
-			Threading.Thread.Sleep(1000)
-			frmTeamScreen.Close()
-		End If
-		'now SplashScreen will show for another 2000ms as MinDisplayTime set to 3000
+    End Sub
 
-		If lblLoadingBar.Text = "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" Then
-			tmrLoadingLabel.Enabled = False 'Stop the progress bar
-		End If
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
 
-	End Sub
-
-	Private Sub Label1_Click(sender As Object, e As EventArgs)
-
-	End Sub
+    End Sub
 
 End Class
